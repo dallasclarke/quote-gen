f8760460bfba99670c37a8bd91b5a179c2e660f6
Dallas Clarke <dallasclarke@Dallass-MacBook-Pro.local>
2021-09-27 22:07:46 -0700

    Able to fetch data from API successfully. I also created newQuote function to pick random quote from json data.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let apiQuotes = [];
 
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const res = await fetch(apiUrl);
+    apiQuotes = await res.json();
+    newQuote();
+  } catch (err) {
+    alert(err);
+  }
+}
+
+getQuotes();
